048e50b4dac099fefe377373b062388f19a7c98d
Nara Kasbergen <bkasbergen@npr.org>
2014-12-12 15:42:29 -0500

    Reworked the plugin a little bit to take advantage of the dynamic autoloader. This also expands the syntax highlighting scripts that are pulled in -- before it was only loading in HTML, CSS and PHP by default.

--- code/SyntaxHighlightedPage.php
+++ code/SyntaxHighlightedPage.php
@@ -1,20 +1,25 @@
 <?php
-class SyntaxHighlightedPage extends DataExtension{
-	
-	function contentcontrollerInit(){
-		
-		//Requirements::javascript($file);
-		
-		//TODO: these javascripts aren"t working:
-		Requirements::javascript(SYNTAX_DIR."/thirdparty/syntaxhighlighter/scripts/shCore.js");
-		Requirements::javascript(SYNTAX_DIR."/thirdparty/syntaxhighlighter/scripts/shBrushPlain.js");
-		Requirements::javascript(SYNTAX_DIR."/thirdparty/syntaxhighlighter/scripts/shBrushPhp.js");
-		Requirements::javascript(SYNTAX_DIR."/thirdparty/syntaxhighlighter/scripts/shBrushJScript.js");
-		Requirements::javascript(SYNTAX_DIR."/thirdparty/syntaxhighlighter/scripts/shBrushCss.js");
-		
-		Requirements::css(SYNTAX_DIR."/thirdparty/syntaxhighlighter/styles/shCore.css");
-		Requirements::css(SYNTAX_DIR."/thirdparty/syntaxhighlighter/styles/shThemeDefault.css");
-		Requirements::customScript("SyntaxHighlighter.all();");
-	}
-	
+
+class SyntaxHighlightedPage extends DataExtension
+{
+    function contentcontrollerInit()
+    {
+        Requirements::javascript("framework/thirdparty/jquery/jquery.js");
+
+        Requirements::css(SYNTAX_DIR . "/thirdparty/syntaxhighlighter/styles/shCore.css");
+        Requirements::css(SYNTAX_DIR . "/thirdparty/syntaxhighlighter/styles/shThemeDefault.css");
+
+        Requirements::javascript(SYNTAX_DIR . "/thirdparty/syntaxhighlighter/scripts/shCore.js");
+        Requirements::javascript(SYNTAX_DIR . "/thirdparty/syntaxhighlighter/scripts/shAutoloader.js");
+        Requirements::javascript(SYNTAX_DIR . "/js/syntaxhighlighter-plugin.js");
+        $syntax_dir = SYNTAX_DIR;
+        Requirements::customScript(<<<JS
+    (function ($) {
+        $(document).ready(function () {
+            $('body').setupSyntaxhighlighter('{$syntax_dir}/thirdparty/syntaxhighlighter/scripts/');
+        });
+    })($);
+JS
+        );
+    }
 }
